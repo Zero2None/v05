917422b3f6c2f4a9df82941c41d2a47a8d7b72c0
Ivan Ceranac <ivcerana@vsite.hr>
2024-12-15 23:05:18 +0100

    Lab 5 resolved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,28 @@
 
 namespace vsite::oop::v5
 {
+    
+    double tire::diameter() {
+        return tireDiameter;
+    }
 
+    int vehicle::year() {
+        return manYear;
+    }
+
+    double product::price() {
+        return value;
+    }
+
+    double car::tire_diameter() {
+        return T.diameter();
+    }
+
+    double operator"" _inch(unsigned long long n) {
+        return n * 2.54;
+    }
+
+    double operator"" _eur(unsigned long long x) {
+        return x * 7.5345;
+    }
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,44 @@
 
 namespace vsite::oop::v5 
 {
+	
+	class tire {
+	public:
+		tire(double d) : tireDiameter(d) {};
+		double diameter();
 
+	private:
+		double tireDiameter;
+	};
+
+	class vehicle {
+	public:
+		vehicle(unsigned m) : manYear(m) {};		
+		int year();
+
+	private:
+		unsigned manYear;
+	};
+
+	class product {
+	public:
+		product(double val) :value(val) {};
+		double price();
+
+	private:
+		double value;
+	};
+
+	class car : public vehicle, public product {
+	public:
+		car(int m, double val, double d) : vehicle(m), product(val), T(d) {};
+		double tire_diameter();
+
+	private:
+		tire T;
+	};
+		
+	double operator"" _inch(unsigned long  long n);
+
+	double operator"" _eur(unsigned long  long x);
 }
